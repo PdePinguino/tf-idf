cc67f341c212b5a02638c98c882f35d07793cba7
melanchthon19 <daniel.mm91@gmail.com>
2021-02-09 21:20:46 -0300

    idf

--- tfidf.py
+++ tfidf.py
@@ -3,28 +3,54 @@
 import json
 import vocab
 
-def total_amount(word, tale):
-    amount = tale.count(word)
-    amount_normalized = amount / len(tale)
+def frequency(word, tale):
+    freq = tale.count(word)
+    freq_normalized = freq / len(tale)
 
-    return amount_normalized
+    return freq_normalized
+
+def frequency_N(word, book):
+    freq = 0
+    for tale in book:
+        freq += book[tale].count(word)
+
+    return freq
 
 def term_frequency(book, vocab):
     # the amount of times that term t occurs in document d
     # normalized by the total amount of occurrences in document d
     tf = {}
     for voc in vocab:
-        tf[voc] = {tale: total_amount(voc, tale) for tale in book}
+        tf[voc] = {tale: frequency(voc, book[tale]) for tale in book}
+
+    check_tf(tf)
 
     return tf
 
-def inverse_document_frequency(book):
-    # total amount of documents divided by
-    # how many times the term t occurs in the set of documents d
+def inverse_document_frequency(book, vocab):
+    # total amount of documents d divided by
+    # the number of documents d that the term t occurs
     idf = {}
-    for tale in book:
-        idf[tale] =2
+    N = len(book.keys())
+    for voc in vocab:
+        idf[voc] = frequency_N(voc, book)
 
+    check_idf(idf)
+
+    return idf
+
+def check_tf(tf):
+    for t in tf:
+        z = [tf[t][tale] for tale in tf[t]]
+        if sum(z) == 0.0:
+            print(tf[t][tale])
+            raise ValueError()
+
+def check_idf(idf):
+    for t in idf:
+        if idf[t] == 0:
+            print(idf[t])
+            raise ValueError()
 
 if __name__ == '__main__':
     with open('text_processed.json', 'r') as openfile:  # loading created json file
@@ -33,6 +59,4 @@ if __name__ == '__main__':
     book = book['book_lemmas']
     vocab = vocab.generate_vocab(book)
     tf = term_frequency(book, vocab[0])
-    #idf = inverse_document_frequency(book)
-    print(tf)
-    #print(idf)
+    idf = inverse_document_frequency(book, vocab[0])
